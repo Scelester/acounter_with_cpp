e11d8cfde83f234b9d497d3410461909f8d90039
nabin paudel <nabin@localhost.localdomain>
2022-05-26 15:06:01 +0545

    sddfsd

---
+++ source/mainAppQ.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QLabel>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QString name = "Accounter";
+    QLabel label1(name);
+    label1.show();
+    return app.exec();
+}
